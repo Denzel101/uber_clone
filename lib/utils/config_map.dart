f670cdf9e29053c957f1957f40611b8ee0981371
Denzel Gatugu <denzelgatugu@outlook.com>
2021-12-09 00:11:27 +0300

    minor update

--- lib/utils/config_map.dart
+++ lib/utils/config_map.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:uber_clone/models/users.dart';
 
-const apiKey = 'AIzaSyDHYaIWVIAUeafkDG95MX4COfELLKgyQkc';
+const apiKey = '<//ADD_API_KEY_HERE\\>';
 
 User? firebaseUser;
 
